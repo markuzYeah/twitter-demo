2f3520cb743948e7ba8fb17f6b321f82837b5a6e
markuz <markuz.yeah@gmail.com>
2013-08-23 01:53:48 +0000

    hopefully fixed the dead twitter socket by reseting the whole process every 1 min

--- cronjob.sh
+++ cronjob.sh
@@ -2,8 +2,13 @@
 
 cd $HOME/work/twitter-demo
 
+forever stopall
+
 forever start -a -c sh sh/grunt-server.sh
 forever start -a -c sh sh/redis.sh
 sleep 2
-forever start -a server/main.js
+forever start -a --minUpTime 1 server/main.js
+forever start -a --minUpTime 1 --spinSleepTime 60000 -c sh sh/restarter.sh
 
+
+forever list
