528a7006848078806bc7360c1c956c1577ab9c80
markuz <markuz.yeah@gmail.com>
2013-08-23 05:08:14 +0000

    saving work

--- cronjob.sh
+++ cronjob.sh
@@ -8,6 +8,8 @@ forever start -c sh sh/grunt-server.sh
 forever start -c sh sh/redis.sh
 sleep 2
 forever start  --minUpTime 1 server/main.js 
+
+# 600_000 = 10 mins
 forever start  --minUpTime 1 --spinSleepTime 600000 -c sh sh/restarter.sh
 
 forever list
